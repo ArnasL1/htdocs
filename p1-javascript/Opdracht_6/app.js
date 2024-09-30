619608bbd571513b3272665e521e54a8b54feb91
ArnasL1 <9012345@student.zadkine.nl>
2024-09-30 15:59:22 +0200

    javascript Opdracht 6

---
+++ p1-javascript/Opdracht_6/app.js
@@ -0,0 +1,17 @@
+
+function calculate(first, second) {
+    const sum = first + second;
+    console.log(sum);
+}
+function multiplyByFive(number) {
+    const sum = number * 5;
+    console.log(sum);
+}
+function calculateMinutes(number) {
+    const sum = number / 60;
+    console.log(sum);
+}
+
+calculate(9, 56);
+multiplyByFive(79);
+calculateMinutes(89000);

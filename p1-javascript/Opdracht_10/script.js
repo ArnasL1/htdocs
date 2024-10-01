a53964703d6f156b39cbbc64fd282c6071c4cc3e
ArnasL1 <9012345@student.zadkine.nl>
2024-10-01 11:17:22 +0200

    javascript opdracht 11

--- p1-javascript/Opdracht_10/script.js
+++ p1-javascript/Opdracht_10/script.js
@@ -4,20 +4,40 @@ let second;
 function add() {
     first = document.getElementById("first").value;
     second = document.getElementById("second").value;
-    answerDisplay.innerHTML = Number(first) + Number(second);
+    if (first == 0 && second == 0) {
+        answerDisplay.innerHTML = "Het getal is te laag"
+    }
+    else {
+        answerDisplay.innerHTML = Number(first) + Number(second);
+    }
 }
 function minus() {
     first = document.getElementById("first").value;
     second = document.getElementById("second").value;
-    answerDisplay.innerHTML = first - second;
+    if (first == 0 && second == 0) {
+        answerDisplay.innerHTML = "Het getal is te laag"
+    }
+    else {
+        answerDisplay.innerHTML = first - second;
+    }
 }
 function multiply() {
     first = document.getElementById("first").value;
     second = document.getElementById("second").value;
-    answerDisplay.innerHTML = first * second;
+    if (first == 0 && second == 0) {
+        answerDisplay.innerHTML = "Het getal is te laag"
+    }
+    else {
+        answerDisplay.innerHTML = first * second;
+    }
 }
 function divide() {
     first = document.getElementById("first").value;
     second = document.getElementById("second").value;
-answerDisplay.innerHTML = first / second;
+    if (first == 0 && second == 0) {
+        answerDisplay.innerHTML = "Het getal is te laag"
+    }
+    else {
+        answerDisplay.innerHTML = first / second;
+    }
 }

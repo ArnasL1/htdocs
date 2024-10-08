e2ceda28733b32952c36b49855793b81415bca1f
ArnasL1 <9012345@student.zadkine.nl>
2024-10-08 10:41:08 +0200

    javascript opdracht 3

---
+++ p2-javascript/Opdracht_3/script.js
@@ -0,0 +1,10 @@
+const text = document.getElementById("text");
+/* Opdracht 1
+for (let i = 1; i <= 100; i++) {
+    text.innerHTML += i + "<br>";
+    
+}
+ Opdracht 2   */
+for (let i = 100; i >= 0; i--) {
+    text.innerHTML += i + "<br>";
+}

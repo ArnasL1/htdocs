f6f47afcad2f9448b24cdb9da7164418185554d4
ArnasL1 <9012345@student.zadkine.nl>
2024-10-14 15:15:42 +0200

    Squashed commit of the following:
    commit a613d2b6cf4bb2cf0192ed5dfee9939d3f1e87a9
    Merge: 9789fc8 1759ffa
    Author: ArnasL1 <9012345@student.zadkine.nl>
    Date:   Mon Oct 14 15:12:07 2024 +0200
    
        Merge branch 'main' into interval
    
    commit 9789fc8c8897da2e215356bd462d3333b5b19efb
    Author: ArnasL1 <9012345@student.zadkine.nl>
    Date:   Mon Oct 14 15:09:27 2024 +0200
    
        javascript opdracht 6
    
    commit 32893d883c0fe186e950ca3e6fab9a46d6fa67d2
    Author: ArnasL1 <9012345@student.zadkine.nl>
    Date:   Mon Oct 14 15:05:42 2024 +0200
    
        javascript opdracht 6

--- p2-javascript/Opdracht_6/script.js
+++ p2-javascript/Opdracht_6/script.js
@@ -3,8 +3,9 @@ let i = 0;
 setInterval(() => {
     i += 10;
     let position = i + "px";
-    block.style.left = position;
-    if (i > 1470) {
+    let width = window.innerWidth;
+        block.style.left = position;
+    if (i > width) {
         i = 0;
     }
 }, 100);

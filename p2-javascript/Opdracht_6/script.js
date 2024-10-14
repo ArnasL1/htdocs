9789fc8c8897da2e215356bd462d3333b5b19efb
ArnasL1 <9012345@student.zadkine.nl>
2024-10-14 15:09:27 +0200

    javascript opdracht 6

--- p2-javascript/Opdracht_6/script.js
+++ p2-javascript/Opdracht_6/script.js
@@ -3,8 +3,8 @@ let i = 0;
 setInterval(() => {
     i += 10;
     let position = i + "px";
-    block.style.left = position;
     let width = window.innerWidth;
+        block.style.left = position;
     if (i > width) {
         i = 0;
     }
